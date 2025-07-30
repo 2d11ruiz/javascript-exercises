4a376eceaa76205c4772293540dea8b481ea9e9e
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-07-30 19:03:47 -0400

    Complete 07_reverseString
    Work on using string and array methods in order to reverse a given
    string, including spaces, numbers, and punctuation.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+/*
+Take in string
+Initialize 'reversed' variable and set equal to:
+    Split string into array of characters
+    Reverse the array
+    Join array of characters into string
+Return 'reversed' variable
+*/
+const reverseString = function(string){
+    let reversed = string.split('').reverse().join('');
+    return reversed;
 };
 
 // Do not edit below this line
