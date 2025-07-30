bdc697c861f3041470d9fccfbe95e34c21a90923
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-07-30 17:17:15 -0400

    Complete 06_repeatString
    Work on using loops to repeat string using string concatenation.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+/*
+Take in string and num
+Initialize newString variable
+For num amount of loops
+    Concatenate string to newString variable
+Return newString
+*/
+const repeatString = function(string, num) {
+    let newString = '';
 
+    for (let i = 0; i < num; i++){
+        newString += string;
+    }
+
+    if (num < 0) return 'ERROR';
+    else return newString; 
 };
 
 // Do not edit below this line
