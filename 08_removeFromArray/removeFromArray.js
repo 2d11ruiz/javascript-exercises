2d49772db5af9621ce5820ed636287a232a8baad
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-08-01 01:35:18 -0400

    Complete 08_removeFromArray
    Work on using loops and array methods to remove all/specific values from
    an array, whether strings, numbers, or both.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+/*
+Take in array and target value array
+Use for of loop to iterate through each target value array item:
+    While target value is present in the array:
+        Get index of target value
+        From array, splice the target at index one time
+Return array
+*/
+const removeFromArray = function(array, ...targets) {
+    for (const target of targets) {
+        while (array.includes(target) === true) {
+            let index = array.indexOf(target);
+            array.splice(index, 1);
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
