a178d39e57bf11678c1148a878290ac152057be5
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-08-05 00:54:11 -0400

    Complete 10_leapYears
    Work on verifying whether the year input is a leap year or not, using
    various conditions.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,29 @@
-const leapYears = function() {
+const leapYears = function(yearIn) {
+    
+    function db4 (year) {
+        if (year % 4 === 0) {
+            return true;
+        }
+    }
 
+    function db100 (year) {
+        if (year % 100 !== 0) {
+            return false;
+        }
+    }
+
+    function db400 (year) {
+        if (year % 400 === 0){
+            return true;
+        }
+    }
+
+    if ((db4(yearIn) === true && db100(yearIn) === false) || db400(yearIn) === true) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
