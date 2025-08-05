d4d64778234bbe6315bc961891c9d00ff3bd2e66
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-08-05 00:05:38 -0400

    Complete 09_sumAll
    Work on retrieving the sum of all numbers between and including two
    given integers, with tests concerning input validation.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    
+    if (startNum < 0 ||
+        endNum < 0 || 
+        typeof startNum !== 'number' ||
+        typeof endNum !== 'number' ||
+        !Number.isInteger(startNum) ||
+        !Number.isInteger(endNum)
+    ) { 
+        return "ERROR";
+    }
+    
+    let tempNum = startNum;
+    if (startNum > endNum) {
+        startNum = endNum;
+        endNum = tempNum;
+    }
 
+    const spanNum = endNum - startNum - 1;
+    let arrayNum = [startNum, endNum];
+
+    for (let i = 0; i < spanNum; i++) {
+        arrayNum.push(startNum + i + 1);
+    }
+
+    const sumNum = arrayNum.reduce((sum, current) => sum + current);
+    return sumNum;
 };
 
 // Do not edit below this line
