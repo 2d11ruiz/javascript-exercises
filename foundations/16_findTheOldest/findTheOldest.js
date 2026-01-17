7a15dad865283a24e2e5cdd96fd715d1c7fb55db
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2026-01-17 00:23:56 -0500

    Complete 16_findTheOldest
    Completed the exercise, which involved finding the oldest person in an
    array of person data objects.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    // Get today's year in case a person has no yearOfDeath property
+    const today = new Date();
+    const year = today.getFullYear();
 
+    return array.reduce((obj, person) => {
+        // If there is no yearOfDeath property for either person being compared, create it and assign today's year to it
+        if (!obj.hasOwnProperty('yearOfDeath')) {
+            obj['yearOfDeath'] = year;
+        } else if (!person.hasOwnProperty('yearOfDeath')) { 
+            person['yearOfDeath'] = year;
+        }
+
+        // If the latter person is older than the former, return the latter person object, else return the former person object
+        if ((person.yearOfDeath - person.yearOfBirth) > (obj.yearOfDeath - obj.yearOfBirth)) {
+            return person;
+        } else {
+            return obj;
+        }
+    });
 };
 
 // Do not edit below this line
